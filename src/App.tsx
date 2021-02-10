99c67e3c18bfb02a13187640f6c9d5ac8a82591d
pattygeek <patienceadajah@gmail.com>
2021-02-09 18:07:13 -0800

    add react-router types

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+
+function App() {
+	return (
+		<div className="App">
+			
+		</div>
+	);
+}
+
+export default App;
